a0f024317ba1634a67b869452247622a4667f047
Avernus <tatesturm13@gmail.com>
2020-01-21 10:38:05 -0600

    added Sprite2D and cursor classes

---
+++ Engine/Engine/World/Sprites2D/Cursor/Cursor.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Graphics;
+using Microsoft.Xna.Framework.Input;
+
+using Engine.World.Sprites2D;
+using Engine.World.GlobalVariables;
+
+namespace Engine.World.Sprites2D.Cursor
+{
+    public class Cursor : Sprite2D
+    {
+
+        public Cursor() : base(new Texture2D(Globals.graphicsDeviceManager.GraphicsDevice, 50, 50), new Vector2(), new Vector2(50), Color.White)
+        {
+
+        }
+
+        public void Update(GameTime _gameTime)
+        {
+
+
+            base.Update(_gameTime);
+        }
+
+    }
+}
---
+++ Engine/Engine/World/Sprites2D/Sprite2D.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Graphics;
+using Microsoft.Xna.Framework.Input;
+
+using Engine.World.GlobalVariables;
+
+namespace Engine.World.Sprites2D
+{
+    public class Sprite2D
+    {
+
+        private Texture2D image;
+        private Vector2 topleft, botRight;
+        private Color color;
+
+        public Sprite2D(Texture2D _image, Vector2 _topLeft, Vector2 _botRight, Color _color)
+        {
+            image = _image;
+            topleft = _topLeft;
+            botRight = _botRight;
+            color = _color;
+        }
+
+        public void Update(GameTime _gameTime)
+        {
+
+        }
+
+        public Texture2D getTexture2D()
+        {
+            return image;
+        }
+
+        public Rectangle getRectangle()
+        {
+            return new Rectangle();
+        }
+
+        public Color getColor()
+        {
+            return color;
+        }
+    }
+}
